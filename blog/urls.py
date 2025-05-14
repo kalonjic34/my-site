b3366c0c4ef2aeeab4cbbc914955077090c85779
Christian <kalonjic34@gmail.com>
2025-05-14 14:03:24 +0200

    Fix typo in variable name for URL patterns in blog/urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 
-urlpattern = [
+urlpatterns = [
     path("",views.starting_page, name="starting-page"),
     path("post",views.post, name="post-page"),
     path("post/<slug:slug>",views.post_detail, name="post-detail-page") #/post/my-first-post
